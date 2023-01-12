72fd4a2b64acfe40dcc9d3c733537171292a1ba4
Manuel Agudelo <juan.manuelillo02@gmail.com>
2023-01-12 11:36:39 -0500

    modificacion del menu lateral en mobile

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
+//----------mobile-----------
+const MenuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktoMenu);
+MenuHamIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktoMenu(){
     console.log('Click');
     DesktopMenu.classList.toggle('inactive');
 }
 
-console.log('Js works!')
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
+}
