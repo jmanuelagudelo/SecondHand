2be4cfbc39b081023a94cea88a1f5e96c763b1d2
Manuel Agudelo <juan.manuelillo02@gmail.com>
2023-01-14 01:31:20 -0500

    implementacion de js para el aside

--- main.js
+++ main.js
@@ -3,15 +3,61 @@ const DesktopMenu = document.querySelector('.desktop-menu');
 //----------mobile-----------
 const MenuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+//----------Shopping-cart-------//
+const ShoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktoMenu);
-MenuHamIcon.addEventListener('click', toggleMobileMenu)
+MenuHamIcon.addEventListener('click', toggleMobileMenu);
+ShoppingCart.addEventListener('click', toggleShopping);
+
+/*---------Reto, dar click en cualquier lugar cierra todos los menus
+
+const mainContainer = document.querySelector('.main-container');
+mainContainer,addEventListener('click', CloseAllMain);
+
+
+function CloseAllMain(){
+    const asideClose = aside.classList.contains('inactive');
+    const mobileMenuClose = mobileMenu.classList.contains('inactive');
+    const DesktopMenuClose = DesktopMenu.classList.contains('inactive');
+
+    if(!asideClose || !mobileMenuClose || !DesktopMenuClose){
+        aside.classList.toggle('inactive');
+        mobileMenu.classList.toggle('inactive');
+        DesktopMenu.classList.toggle('inactive');
+    }
+}
+*/
+//--funciones--menus-----//
 
 function toggleDesktoMenu(){
-    console.log('Click');
+    const asideClose = aside.classList.contains('inactive');
+
+    if(!asideClose){
+        aside.classList.add('inactive');
+    }
+
     DesktopMenu.classList.toggle('inactive');
+
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    const asideClose = aside.classList.contains('inactive');
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShopping(){
+    const mobileMenuClose = mobileMenu.classList.contains('inactive');
+    const DesktopMenuClose = DesktopMenu.classList.contains('inactive');
+
+    if(!DesktopMenuClose){
+        DesktopMenu.classList.add('inactive');
+    }
+
+    if(!mobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
