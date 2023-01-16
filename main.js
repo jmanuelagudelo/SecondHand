bde0899d8324da203a4181bda655ee3d48d54f07
Manuel Agudelo <juan.manuelillo02@gmail.com>
2023-01-16 17:27:40 -0500

    Maquetando los articulos en js mediante el uso de arrays

--- main.js
+++ main.js
@@ -11,10 +11,14 @@ menuEmail.addEventListener('click', toggleDesktoMenu);
 MenuHamIcon.addEventListener('click', toggleMobileMenu);
 ShoppingCart.addEventListener('click', toggleShopping);
 
-/*---------Reto, dar click en cualquier lugar cierra todos los menus */
+/*---- dar click en cualquier lugar cierra todos los menus----- */
 
 const mainContainer = document.querySelector('.main-container');
 mainContainer.addEventListener('click', CloseAllMain);
+
+/*--------------- Contenedor de productos --------*/
+
+const cardsContainer = document.querySelector('.cards-container');
 
 
 function CloseAllMain(){
@@ -68,3 +72,100 @@ function toggleShopping(){
     }
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Hoodie Weed-Green',
+    price: 250.00,
+    image: "./sources/Images/IMG_2914 Large.jpeg",
+});
+
+productList.push({
+    name: 'Hoodie classic blue',
+    price: 200.00,
+    image: "./sources/Images/IMG_2912 Large.jpeg"
+});
+
+productList.push({
+    name: "Hoodie Yellow stripes",
+    price: 180.00,
+    image: "./sources/Images/IMG_2916 Large.jpeg"
+});
+
+productList.push({
+    name: "Bowling stripes shirt",
+    price: 100.00,
+    image: "./sources/Images/IMG_2918 Large.jpeg"
+});
+
+productList.push({
+    name: "Japanisse Shirt design",
+    price: 120.00,
+    image: "./sources/Images/IMG_2921 Large.jpeg"
+});
+
+productList.push({
+    name: "Psychodelic Shirt design",
+    price: 120.00,
+    image: "./sources/Images/IMG_2923 Large.jpeg"
+});
+
+productList.push({
+    name: "Leaves shirt design",
+    price: 120.00,
+    image: "./sources/Images/IMG_2925 Large.jpeg"
+});
+
+productList.push({
+    name:  "Peachish Diamonds Sweeter",
+    price: 150.00,
+    image: "./sources/Images/IMG_2929 Large.jpeg"
+});
+
+productList.push({
+    name: "Star wars Sweeter",
+    price: 150.00,
+    image: "./sources/Images/IMG_2927 Large.jpeg"
+});
+
+productList.push({
+    name: "Blue Diamonds Sweeter",
+    price: 150.00,
+    image: "./sources/Images/IMG_2931 Large.jpeg"
+});
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.append(productPrice, productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './sources/icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.append(productInfoDiv,productInfoFigure);
+    
+        productCard.append(productImg, productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+renderProducts(productList);
