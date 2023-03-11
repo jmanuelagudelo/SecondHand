ab9597f9baa9211e150f7f26fb99aff4bb1cc236
Manuel Agudelo <juan.manuelillo02@gmail.com>
2023-03-10 22:23:42 -0500

    ajuste toggle mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import '@styles/Header.scss';
 import DesktopMenu from '@components/Menu';
+import Mobilemenu from '@components/mobilemenu';
 import logo from '@logos/logo-second-hand.svg'
 import IconMenu from '@icons/icon_menu.svg'
 import ShoppingImg from '@icons/icon_shopping_cart.svg'
@@ -12,9 +13,13 @@ const Header = () => {
     const handdleToggle = () => {
         setToggle(!toggle);
     }
+    const [mobilemenu, setMenu] = useState(false);
+    const handdleMenu = () => {
+        setMenu(!mobilemenu);
+    }
     return(
         <nav className="main-nav">
-        <img src={IconMenu} alt="icon_menu" className="menu"/>
+        <img src={IconMenu} alt="icon_menu" className="menu" onClick={handdleMenu}/>
 
         <div className="navbar-left">
             <img src={logo} alt="logo" className="logo-nav"/>
@@ -47,6 +52,7 @@ const Header = () => {
         </ul>
         </div>
         {toggle ? <DesktopMenu/> : null}
+        {mobilemenu ? <Mobilemenu/> : null}
     </nav>
     );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, {useRef} from "react";
 import '../styles/Login.scss';
 import Logo from '../logos/logo-second-hand.svg';
 
 
 const Login = () => {
+    const loginform = useRef(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const loginData = new FormData(loginform.current);
+        const data = {
+            username: loginData.get('email'),
+            password: loginData.get('password')
+        }
+        console.log(data);
+    }
     return(
         <div className  ="Login ">
             <div className ="Login-container">
                 <img src={Logo} alt="logo" className ="logo"/>
-                <form action="/" className ="form">
+                <form action="/" className ="form" ref={loginform}>
                     <label for="email" className="label">Email address</label>
-                    <input type="text" id="email" placeholder="********" className="input input-email"/>
+                    <input type="text" name="email" placeholder="********" className="input input-email"/>
 
 					<label for="password" className="label">Password</label>
-					<input type="password" id="password" placeholder="*********" className="input input-password" />
+					<input type="password" name="password" placeholder="*********" className="input input-password" />
                     
-                    <input type="submit" value="Log in" className="primary-button login-button" />
+                    <button type="submit"className="primary-button login-button"
+                    onClick={handleSubmit}>
+                    Log in
+                    </button>
                     <a href="/">Forgot my password</a>
                 </form>
 
-                <button className ="secondary-button sign-button bot-ton">Sign up</button>
+                <button className ="secondary-button sign-button bot-ton" >
+                Sign up
+                </button>
 
             </div>
         </div>
